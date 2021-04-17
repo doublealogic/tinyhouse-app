ccfbe111a96a10591b8d0d3fddffec385aba15d7
doublealogic <cras.ajrobertson@gmail.com>
2021-04-17 14:54:31 -0400

    updated index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -15,6 +15,18 @@ app.get("/listings", (_req, res) => {
     return res.send(listings);
 });
 
+app.post("/delete-listing", (req, res) => {
+    const id: string = req.body.id;
+
+    for (let i = 0; i < listings.length; i++) {
+        if (listings[i].id === id) {
+            return res.send(listings.splice(i, 1));
+        }
+    }
+
+    return res.send("failed to delete listing");
+});
+
 app.listen(port);
 
 console.log(`[app]: http://localhost:${port}`);
