b5994b9d50032dc3207597e7bea51272448d7387
doublealogic <cras.ajrobertson@gmail.com>
2021-04-17 14:49:40 -0400

    Updated index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,19 @@
 import express = require('express');
+import {listings} from "./listings";
+
 const app = express();
 const port = 9000;
+
+app.use(express.json());
 
 const one = 1;
 const two = 2;
 
 app.get("/", (_req, res) => res.send(`1 + 2 = ${one + two}`));
+
+app.get("/listings", (_req, res) => {
+    return res.send(listings);
+});
 
 app.listen(port);
 
