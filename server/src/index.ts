9624fc5e72675a941fa95c82d1d5b5106ba31c87
doublealogic <cras.ajrobertson@gmail.com>
2021-04-17 16:14:36 -0400

    installed apollo and graphQl

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,11 +6,6 @@ const port = 9000;
 
 app.use(express.json());
 
-const one = 1;
-const two = 2;
-
-app.get("/", (_req, res) => res.send(`1 + 2 = ${one + two}`));
-
 app.get("/listings", (_req, res) => {
     return res.send(listings);
 });
