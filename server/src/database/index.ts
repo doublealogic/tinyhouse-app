d3de11a197fd1628bbac2cec358129c46c689b5b
doublealogic <cras.ajrobertson@gmail.com>
2021-04-20 07:43:45 -0400

    Added database folder, installed mongodb

---
+++ server/src/database/index.ts
@@ -0,0 +1,9 @@
+import { mongoClient } from "mongodb";
+
+const user = '';
+const userPassword = '';
+const cluster = '';
+const url = 
+    "mongodb+srv://user_001:<password>@cluster0.sp51p.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+export const connectDatabase = () => {};
