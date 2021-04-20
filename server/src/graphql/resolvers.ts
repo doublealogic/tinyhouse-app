dc95bc946235c4667fc7271a00955eab14310fa1
doublealogic <cras.ajrobertson@gmail.com>
2021-04-20 07:12:00 -0400

    Updated files

--- server/src/graphql/resolvers.ts
+++ server/src/graphql/resolvers.ts
@@ -1,13 +1,13 @@
 import { listings } from '../listings';
 
-export const resolvers = {
+export const resolvers: IResolvers = {
     Query: {
         listings: () => {
             return listings;
         }
     },
     Mutation: {
-        deleteListing: (_root, { id }) => {
+        deleteListing: (_root: undefined, { id}: { id: string }) => {
             for (let i = 0; i < listings.length; i++) {
                 if (listings[i].id === id) {
                     return listings.splice(i, 1)[0];
