5cccd8d67ce905ff5269796bc9cc08fc7f9a977c
doublealogic <cras.ajrobertson@gmail.com>
2021-04-20 07:18:05 -0400

    updated index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,11 @@
 import express = require('express');
 import {ApolloServer} from 'apollo-server-express';
-import { typeDefs, resolvers, schema} from './graphql';
+import { typeDefs, resolvers} from './graphql';
 
 const app = express();
 const port = 9000;
 
-const server = new ApolloServer({ schema });
+const server = new ApolloServer({ typeDefs, resolvers });
 server.applyMiddleware({app, path: '/api'});
 
 app.use(express.json());
