afaa74241fd943fea566e1bb4ce74243bda189c0
doublealogic <cras.ajrobertson@gmail.com>
2021-04-20 10:06:31 -0400

    Updated resolvers.ts

--- server/src/graphql/resolvers.ts
+++ server/src/graphql/resolvers.ts
@@ -1,21 +1,28 @@
+import { ObjectId } from "mongodb";
 import { IResolvers } from "apollo-server-express";
-import { listings } from '../listings';
+import { listings } from "../listings";
+import { Database } from "../lib/types";
+
 
 export const resolvers: IResolvers = {
     Query: {
-        listings: () => {
-            return listings;
+        listings: async (_root: undefined, _args: {}, { db }: { db: Database }) => {
+            return await db.listings.find({}).toArray();
         }
     },
     Mutation: {
-        deleteListing: (_root: undefined, { id}: { id: string }) => {
-            for (let i = 0; i < listings.length; i++) {
-                if (listings[i].id === id) {
-                    return listings.splice(i, 1)[0];
+        deleteListing: async (
+            _root: undefined, 
+            { id}: { id: string }, 
+            { db }: { db: Database }
+            ) => {
+                const deleteRes = await db.listings.findOneAndDelete({ 
+                    _id: new ObjectId(),
+                });
+                
+                if (!deleteRes.value) {
+                    throw new Error('failed to delete listing');
                 }
-            }
-
-            throw new Error("failed to delete listing");
         }
     }
 };
