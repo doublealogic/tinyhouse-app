ad3245b258fb8606f50de528e2b830863a84b03c
doublealogic <cras.ajrobertson@gmail.com>
2021-04-19 23:12:14 -0400

    Updated graphql.ts

--- server/src/graphql.ts
+++ server/src/graphql.ts
@@ -1,10 +1,35 @@
-import { GraphQLObjectType, GraphQLSchema, GraphQLString } from "graphql";
+import { 
+    GraphQLID,
+    GraphQLInt,
+    GraphQLList,
+    GraphQLNonNull,
+    GraphQLObjectType, 
+    GraphQLSchema, 
+    GraphQLString 
+} from "graphql";
+
+import { listings } from "./listings"
+
+const Listing = new GraphQLObjectType({
+    name: "Listing",
+    fields: {
+        id { type: GraphQLNonNull(GraphQLID) },
+        title: { type: GraphQLNonNull(GraphQLString) },
+        image: { type: GraphQLNonNull(GraphQLString) },
+        address: { type: GraphQLNonNull(GraphQLString) },
+        price: { type: GraphQLNonNull(GraphQLInt) },
+        numOfGuests: { type: GraphQLNonNull(GraphQLInt) },
+        numOfBeds: { type: GraphQLNonNull(GraphQLInt) },
+        numOfBaths: { type: GraphQLNonNull(GraphQLInt) },
+        rating: { type: GraphQLNonNull(GraphQLInt) }
+    }
+});
 
 const query = new GraphQLObjectType({
     name: 'Query',
     fields: {
-        hello: { 
-            type: GraphQLString,
+        listings: { 
+            type: GraphQLNonNull(GraphQLList(GraphQLNonNull(Listing))),
             resolve: () => "Hello from the Query"
         }
     }
@@ -13,9 +38,20 @@ const query = new GraphQLObjectType({
 const mutation = new GraphQLObjectType({
     name: "Mutation",
     fields: {
-        hello: { 
-            type: GraphQLString,
-            resolve: () => "Hello from the Mutation!"
+        deleteListing: { 
+            type: GraphQLNonNull(Listing),
+            args: {
+                id: { type: GraphQLNonNull(GraphQLID) }
+            },
+            resolve: (_root, { id } ) => {
+                for (let i = 0; i < listings.length; i++) {
+                    if (listings[i].id === id) {
+                        return listings.splice(i, 1)[0];
+                    }
+                }
+
+                throw new Error("failed to delete listing");
+            }
         }
     }
 });
