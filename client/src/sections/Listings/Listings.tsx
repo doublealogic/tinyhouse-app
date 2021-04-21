83c1b28c972b048c57450ac48a157f5fd2c63ff4
doublealogic <cras.ajrobertson@gmail.com>
2021-04-21 06:13:47 -0400

    Updated listings.tsx, added lib folder with api folder inside

--- client/src/sections/Listings/Listings.tsx
+++ client/src/sections/Listings/Listings.tsx
@@ -1,9 +1,35 @@
 import React from "react";
+import { server } from "../../lib/api";
+
+const LISTINGS = `
+    query Listings {
+        listings {
+            id
+            title
+            image
+            address
+            price
+            numOfGuests
+            numOfBeds
+            rating
+        }
+    }
+`;
 
 interface Props {
     title: string;
 }
 
 export const Listings = ({ title }: Props) => {
-    return <h2>{title}</h2>;
+    const fetchListings = async () => {
+        const listings = await server.fetch({ query: LISTINGS});
+        console.log(listings)
+    };
+
+    return (
+    <div>
+        <h2>{title}</h2>
+        <button onClick={fetchListings}>Query Listings!</button>
+    </div>
+    );
 };
