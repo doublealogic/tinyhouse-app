c2fa126f0dfdb0125b39317f4b0792031d305463
doublealogic <cras.ajrobertson@gmail.com>
2021-04-21 06:36:37 -0400

    Updated listings.tsx file

--- client/src/sections/Listings/Listings.tsx
+++ client/src/sections/Listings/Listings.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { server } from "../../lib/api";
+import { ListingsData } from "./types";
 
 const LISTINGS = `
     query Listings {
@@ -22,7 +23,8 @@ interface Props {
 
 export const Listings = ({ title }: Props) => {
     const fetchListings = async () => {
-        const data = await server.fetch({ query: LISTINGS });
+        const data = await server.fetch<ListingsData>({ query: 
+            LISTINGS });
         console.log(data);
     };
 
