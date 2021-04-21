037d598d38728ae612ddbe2dc24f0231a7610b68
doublealogic <cras.ajrobertson@gmail.com>
2021-04-21 04:40:18 -0400

    Updated database index.ts

--- server/src/database/index.ts
+++ server/src/database/index.ts
@@ -1,11 +1,12 @@
 import { MongoClient } from "mongodb";
+import { Database } from "../lib/types";
 
 const url = 
     `mongodb+srv://${process.env.DB_USER}:${
         process.env.DB_USER_PASSWORD
     }@${process.env.DB_CLUSTER}.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
-export const connectDatabase = async () => {
+export const connectDatabase = async (): Promise<Database> => {
     const client = await MongoClient.connect(url, {
         useNewUrlParser: true,
         useUnifiedTopology: true
