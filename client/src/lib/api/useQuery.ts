e07d5cf87ffd2d69ab83afdc35c72efc84850133
doublealogic <cras.ajrobertson@gmail.com>
2021-04-23 15:00:21 -0400

    Updated useQuery.ts

--- client/src/lib/api/useQuery.ts
+++ client/src/lib/api/useQuery.ts
@@ -4,21 +4,28 @@ import { server } from './server';
 interface State<TData> {
     data: TData | null;
     loading: boolean;
+    error: boolean
 }
 
 export const useQuery = <TData = any>(query: string) => {
     const [state, setState] = useState<State<TData>>({
         data: null,
-        loading: false
+        loading: false,
+        error: false
     });
 
     const fetch = useCallback(() => {
         const fetchApi = async () => {
-            setState({ data: null, loading: true });
+            try {
+                setState({ data: null, loading: true, error: false });
 
-            const { data } = await server.fetch<TData>({ query });
+                const { data } = await server.fetch<TData>({ query });
 
-            setState({ data, loading: false });
+                setState({ data, loading: false, error: false });
+            } catch (err) {
+                setState({ data: null, loading: false, error: true });
+                throw console.error(err);
+            }
         };
 
         fetchApi();
