4e3a672e7307e2d10476a29a18716fb35fa1bbe3
doublealogic <cras.ajrobertson@gmail.com>
2021-04-23 15:08:43 -0400

    Updated listings.tsx

--- client/src/sections/Listings/Listings.tsx
+++ client/src/sections/Listings/Listings.tsx
@@ -34,7 +34,7 @@ interface Props {
 }
 
 export const Listings = ({ title }: Props) => {
-    const { data, loading, refetch } = useQuery<ListingsData>(LISTINGS);
+    const { data, loading, error, refetch } = useQuery<ListingsData>(LISTINGS);
 
     const deleteListing = async (id: string) => {
         await server.fetch<
@@ -71,6 +71,14 @@ export const Listings = ({ title }: Props) => {
 
     if (loading) {
         return <h2>Loading...</h2>;
+    }
+
+    if (error) {
+        return (
+            <h2>
+                Uh oh! Something went wrong. Please try again later!
+            </h2>
+        );
     }
 
     return (
