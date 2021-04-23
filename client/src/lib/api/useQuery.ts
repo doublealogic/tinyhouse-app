ce42efaf07e7f714fe4eed55f707f7ae5e757d86
doublealogic <cras.ajrobertson@gmail.com>
2021-04-23 14:10:03 -0400

    Finalized useQuery.ts

--- client/src/lib/api/useQuery.ts
+++ client/src/lib/api/useQuery.ts
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { server } from './server';
 
 interface State<TData> {
     data: TData | null;
@@ -8,4 +9,15 @@ export const useQuery = <TData = any>(query: string) => {
     const [state, setState] = useState<State<TData>>({
         data: null
     });
+
+    useEffect(() => {
+        const fetchApi = async () => {
+            const { data } = await server.fetch<TData>({ query });
+            setState({ data });
+        };
+
+        fetchApi();
+    }, [query]);
+
+    return state;
 };
