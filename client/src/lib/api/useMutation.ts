6b07e56a177c7cb3d7b009af62c28bb5d71cbc86
doublealogic <cras.ajrobertson@gmail.com>
2021-04-24 14:50:07 -0400

    Added and updated useMutation.ts file

---
+++ client/src/lib/api/useMutation.ts
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+import { server } from "./server";
+
+interface State<TData> {
+    data: TData | null;
+    loading: boolean;
+    error: boolean;
+}
+
+export const useMutation = <TData = any, TVariables = any>(
+    query: string
+) => {
+    const [state, setState] = useState<State<TData>>({
+        data: null,
+        loading: false,
+        error: false
+    });
+
+    const fetch = async (variables?: TVariables) => {
+        try { 
+            setState({ data: null, loading: true, error: false });
+
+            const { data, errors } = await server.fetch<
+                TData,
+                TVariables
+            >({ query, variables });
+
+            if (errors && errors.length) {
+                throw new Error(errors[0].message);
+            }
+
+            setState({ data, loading: false, error: false});
+        } catch (err) {
+            setState({ data: null, loading: false, error: true });
+            throw console.error(err);
+        }
+    };
+
+    return [fetch, state];
+};
