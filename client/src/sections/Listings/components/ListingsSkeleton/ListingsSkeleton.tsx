91e6c2dc01c60af233843af2558d0440f8cfeadb
doublealogic <cras.ajrobertson@gmail.com>
2021-04-27 22:26:56 -0400

    Updated ListingsSkeleton with error alert

--- client/src/sections/Listings/components/ListingsSkeleton/ListingsSkeleton.tsx
+++ client/src/sections/Listings/components/ListingsSkeleton/ListingsSkeleton.tsx
@@ -1,14 +1,24 @@
 import React from "react";
-import { Divider, Skeleton } from "antd";
+import { Alert, Divider, Skeleton } from "antd";
 import "./styles/ListingsSkeleton.css";
 
 interface Props {
     title: string;
+    error?: boolean;
 }
 
-export const ListingsSkeleton = ({ title }: Props) => {
+export const ListingsSkeleton = ({ title, error = false }: Props) => {
+    const errorAlert = error ? (
+        <Alert
+            type="error"
+            message="Uh oh! Something went wrong. Please try again later."
+            className="listings-skeleton__alert"
+        />
+    ) : null;
+
     return (
         <div className="listing-skeleton">
+            {errorAlert}
             <h2>{title}</h2>
             <Skeleton active paragraph={{ rows: 1 }} />
             <Divider />
